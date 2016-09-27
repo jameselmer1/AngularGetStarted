26db2375438af3a69aee86c2e1c953bc981f48af
james.elmer@live.com <james.elmer@live.com>
2016-09-27 17:19:42 -0400

    ng-repeat

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 // Code goes here
 (function () {
-  var app = angular.module("githubViewer", []);
+    var app = angular.module("githubViewer", []);
 
-  var MainController = function ($scope, $http) {
-    var onUserComplete = function (response) {
-      $scope.user = response.data;
+    var MainController = function ($scope, $http) {
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
+        };
+
+        var onRepos = function(response) {
+            $scope.repos = response.data;
+        };
+
+        var onError = function (reason) {
+            $scope.error = "Could not get user";
+        };
+
+        $scope.search = function (username) {
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError);
+        };
+
+        $scope.username = "angular";
+        $scope.message = "It turns out Harold's not a jedi";
     };
 
-    var onError = function (reason) {
-      $scope.error = "Could not get user";
-    };
-    
-    $scope.search = function(username){
-        $http.get("https://api.github.com/users/" + username)
-            .then(onUserComplete, onError);  
-    };
 
-      $scope.username = "angular";
-    $scope.message = "It turns out Harold's not a jedi";
-  };
-
-
-  app.controller("MainController", ["$scope", "$http", MainController]);
-} ());
+    app.controller("MainController", ["$scope", "$http", MainController]);
+}());
