0323a2e1f0b113922cee01c3ae0759cc3be976f7
James <james.elmer@live.com>
2016-10-09 11:22:08 -0400

    check in

--- angularScript.js
+++ angularScript.js
@@ -1,29 +1,42 @@
-// Code goes here
 (function () {
     var app = angular.module("githubViewer", []);
 
     var MainController = function ($scope, $http) {
+
         var onUserComplete = function (response) {
             $scope.user = response.data;
+            $scope.userDetailsView = 'userDetails.html';
             $http.get($scope.user.repos_url)
                 .then(onRepos, onError);
         };
 
         var onRepos = function(response) {
             $scope.repos = response.data;
+            $scope.userReposView = 'userRepos.html';
         };
 
         var onError = function (reason) {
             $scope.error = "Could not get user";
         };
 
+        $scope.hideError = function() {
+            $scope.error = "";
+        }
+
         $scope.search = function (username) {
             $http.get("https://api.github.com/users/" + username)
                 .then(onUserComplete, onError);
         };
 
-        $scope.username = "angular";
-        $scope.message = "It turns out Harold's not a jedi";
+        $scope.username = "robconery";
+        $scope.message = "Search Github";
+        $scope.error = "";
+        $scope.userDetailsView = "";
+        $scope.userReposView = "";
+        
+        $scope.sortName = 'stargazers_count';   // set the default sort column name
+        $scope.sortReverse = true;             // set the default sort order
+        $scope.searchRepos = '';                // set the default search/filter term
     };
 
 
